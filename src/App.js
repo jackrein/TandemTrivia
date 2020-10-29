56960561e61e69534993cd149118909cf90166e5
Jackson Reinagel <jackrein@users.noreply.github.com>
2020-10-29 14:46:02 -0600

    Set current q w/useState, next q onClick

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import qBank from './Trivia_Data.json';
 
 export default function App() {
+  const [currentQ, setCurrentQ] = useState(0);
+
+  const handleAnswerClick = (answerOption) => {
+    const nextQ = currentQ + 1;
+    if (nextQ < qBank.length) {
+      setCurrentQ(nextQ);
+    } else {
+      alert('you\'ve reached the end of the game');
+    }
+  };
+
   return (
 		<div className='app'>
-      <div className='question-text'>{qBank[0].question}</div>
+      <div className='question-text'>{qBank[currentQ].question}</div>
       <div className='answer-section'>
-        {qBank[0].incorrect.concat(qBank[0].correct).map((answerOption, index) => (
-		      <button>{answerOption}</button>
+        {qBank[currentQ].incorrect.concat(qBank[currentQ].correct).map((answerOption, index) => (
+		      <button onClick={() => handleAnswerClick()}>{answerOption}</button>
       	))}
       </div>
     </div>
