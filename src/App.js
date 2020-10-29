d874a7db017443d3e12ab1ca275e5bf0ddd43db5
Jackson Reinagel <jackrein@users.noreply.github.com>
2020-10-29 12:17:07 -0600

    Set up src files
    Replace CRA content with button-style Q&A

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+		<div className='app'>
+      <div className='question-text'>This is where the question text will go</div>
+      <div className='answer-section'>
+        <button>Answer 1</button>
+        <button>Answer 2</button>
+        <button>Answer 3</button>
+        <button>Answer 4</button>
+      </div>
     </div>
   );
 }
-
-export default App;
