4f108aba14bff8c6f33173d77e54d3ad183be2cf
Jackson Reinagel <jackrein@users.noreply.github.com>
2020-10-30 14:50:46 -0600

    Add logic to randomize q's
    Change from asking first 10 questions in bank to asking 10 random, unique questions

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import qBank from './Trivia_Data.json';
 import logo from './TandemTrivia Logo.png'
 
 export default function App() {
+  const [questions, getQuestions] = useState(qBank.sort(() => 0.5 - Math.random()).slice(0, 10));
   const [currentQ, setCurrentQ] = useState(0);
   const [showScore, setShowScore] = useState(false);
   const [score, setScore] = useState(0);
 
   const handleAnswerClick = (answerOption) => {
-    let rightAnswer = qBank[currentQ].correct;
+    let rightAnswer = questions[currentQ].correct;
     if (answerOption === rightAnswer) {
       setScore(score + 1);
       alert('That\'s correct!');
@@ -16,7 +17,7 @@ export default function App() {
       alert('I\'m sorry, the correct answer is ' + rightAnswer);
     }
     const nextQ = currentQ + 1;
-    if (nextQ < 10) {
+    if (nextQ < questions.length) {
       setCurrentQ(nextQ);
     } else {
       setShowScore(true);
@@ -35,10 +36,10 @@ export default function App() {
               <div className='qCount'>
                 <span>Question {currentQ+1}</span>/10
               </div>
-              <div className='qText'>{qBank[currentQ].question}</div>
+              <div className='qText'>{questions[currentQ].question}</div>
             </div>
             <div className='answer-section'>
-              {qBank[currentQ].incorrect.concat(qBank[currentQ].correct).map((answerOption, index) => (
+              {questions[currentQ].incorrect.concat(questions[currentQ].correct).map((answerOption, index) => (
                 <button onClick={() => handleAnswerClick(answerOption)}>{answerOption}</button>
               ))}
             </div>
