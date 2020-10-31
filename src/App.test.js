bc15593494830ce8ca8d46c548847cc4f0be51ca
Jackson Reinagel <jackrein@users.noreply.github.com>
2020-10-31 12:05:02 -0600

    Add tests: renders, renders logo, restarts
    Currently passing all but "restarts game" test. Need to refactor into components and then test PlayBtn as a unit (plus an integration test).

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { restartGame } from './App';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
 });
+
+test('renders Tandem Trivia logo', async () => {
+  render(<App />);
+  const logo = await screen.findByAltText(/Tandem Trivia logo/i);
+  expect(logo).toBeInTheDocument();
+});
+
+test('restarts game and renders new Question 1', () => {
+  render(<App />);  // Need to refactor with PlayBtn as component
+  userEvent.click(screen.getByText('Play Again'));
+  expect(restartGame).toHaveBeenCalledTimes(1);
+  expect(screen.getByText(/Question 1/i)).toBeInTheDocument();
+})
